8a5347d976e0efa86dec1989a8e35e737b66d8ef
Salah Eddine Taouririt <tarrsalah@gmail.com>
2020-10-21 14:44:29 +0100

    stop and start.

--- lib/app_web/controllers/page_controller.ex
+++ lib/app_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule AppWeb.PageController do
   use AppWeb, :controller
 
   def index(conn, _params) do
-    conn |> send_resp(:ok, "deployed!")
+    conn |> send_resp(:ok, "deployed with a github action ^_^.")
   end
 end
